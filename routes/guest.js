9a0099811dad18b64b9ff37b44f72dff69e8a768
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2021-04-30 18:11:14 -0700

    Login/Create routes

--- routes/guest.js
+++ routes/guest.js
@@ -1,7 +1,52 @@
 const router = require('express').Router();
+const db = require("../functions/db")
 
 router.get('/', (req, res) => {
     res.send("From guest.js");
+});
+
+router.get('/login', (req, res) => {
+    res.render("login");
+});
+
+router.post('/login', async (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let error = "ERROR: Incorrect password";
+
+    let sql = "SELECT * FROM guests WHERE username = ?";
+    let params = [username];
+    let rows = await db.executeSQL(sql, params);
+
+    console.log(rows);
+
+    if (rows.length <= 0){
+        error = "ERROR: Username not found";
+    }
+    
+
+    if (password == rows[0].password){
+        res.send("success");
+    } else {
+        res.render("login", {"error":error});
+    }
+});
+
+router.post('/create', async (req, res) => {
+    let fName = req.body.fName;
+    let lName = req.body.lName;
+    let username = req.body.username;
+    let password = req.body.password;
+
+    let sql = "INSERT INTO guests (firstName, lastName, username, password) VALUES (?, ?, ?, ?)";
+    let params = [fName, lName, username, password];
+    let rows = await db.executeSQL(sql, params);
+
+    // TODO: Sanitize input + check for existing username (in hindsight username probably could be the primary key of the table?)
+
+
+    res.render("login", {"error":`Account ${username} created successfully!`});
 });
 
 module.exports = router;
