5252204e3ec37bbb2b5b4a7333842bddcbf39caa
James Campbell <jscampbell43@gmail.com>
2021-05-08 18:56:07 -0700

    commit workspace

--- routes/admin.js
+++ routes/admin.js
@@ -18,12 +18,36 @@ router.get('/rooms/', async (req, res) => {
     res.render('admin', {"keyWord":"rooms", table})
 })
 
-router.get('/rooms/update', async (req, res) =>{
-    let guestId = req.query.authorId;
-    let sql = 'SELECT * FROM guests WHERE authorId = ?';
-    let params = [guestId];
+router.get('/room/update/', async (req, res) =>{
+    let roomId = req.query.roomId;
+    console.log(roomId);
+    let sql = 'SELECT * FROM rooms WHERE roomId = ?';
+    let params = [roomId];
     let rows = await db.executeSQL(sql, params);
-    res.render('roomUpdate', {"guest":rows[0]});
+    console.log(rows);
+    res.render('roomUpdate', {"room":rows[0]});
+})
+
+router.post('/room/update/', async (req, res)=>{
+    console.log("In Post");
+    let roomId = req.body.roomId;
+    let type = req.body.type;
+    let price = req.body.price;
+    let image = req.body.image;
+    let descript = req.body.descript;
+
+    let sql = 'UPDATE rooms SET type = ?, price = ?, image = ?, description = ? WHERE roomId = ${roomId}';
+    let params = [type, price, image, descript];
+    let rows = await db.executeSQL(sql, params);
+    console.log("ROWS: "+ rows);
+    console.log("RoomID: " + roomId);
+    //res.redirect(`/rooms/update?roomId=${roomId}`);
+
+    let sql2 = 'SELECT * FROM rooms WHERE roomId = ?';
+    let params2 = [roomId];
+    let rows2 = await db.executeSQL(sql2, params2);
+    console.log(rows);
+    res.render('roomUpdate', {"room":rows2[0]});
 })
 
 router.get('/reservations/', async (req, res) => {
